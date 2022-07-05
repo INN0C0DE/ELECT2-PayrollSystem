9f754e961f5a0c0faaa6f9836860190c45347a87
Raphael Arnaldo Cruz <53103201+INN0C0DE@users.noreply.github.com>
2022-07-05 16:46:15 +0800

    Add files via upload

---
+++ MidtermExam/MidtermExam/Form1.Designer.vb
@@ -0,0 +1,308 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.tb1 = New System.Windows.Forms.TextBox()
+        Me.tb2 = New System.Windows.Forms.TextBox()
+        Me.tb3 = New System.Windows.Forms.TextBox()
+        Me.tb4 = New System.Windows.Forms.TextBox()
+        Me.tb5 = New System.Windows.Forms.TextBox()
+        Me.tb6 = New System.Windows.Forms.TextBox()
+        Me.tb7 = New System.Windows.Forms.TextBox()
+        Me.tb8 = New System.Windows.Forms.TextBox()
+        Me.tb9 = New System.Windows.Forms.TextBox()
+        Me.tb10 = New System.Windows.Forms.TextBox()
+        Me.tb11 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(37, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(123, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Employee's Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(31, 71)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(129, 16)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "No. of hours worked:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(81, 100)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 16)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Net Income:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(52, 128)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(108, 16)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Overtime Hour/s:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(60, 156)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(103, 16)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Overtime Fee/s:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(125, 184)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(38, 16)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "SSS:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(101, 212)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(62, 16)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Pag-Ibig:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(91, 240)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(72, 16)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "PhilHealth:"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(123, 268)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(37, 16)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "TAX:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label10.Location = New System.Drawing.Point(57, 296)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(106, 16)
+        Me.Label10.TabIndex = 9
+        Me.Label10.Text = "Total Deduction:"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(101, 343)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(59, 16)
+        Me.Label11.TabIndex = 10
+        Me.Label11.Text = "Net Pay:"
+        '
+        'tb1
+        '
+        Me.tb1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tb1.Location = New System.Drawing.Point(166, 24)
+        Me.tb1.Name = "tb1"
+        Me.tb1.Size = New System.Drawing.Size(190, 22)
+        Me.tb1.TabIndex = 11
+        '
+        'tb2
+        '
+        Me.tb2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tb2.Location = New System.Drawing.Point(166, 68)
+        Me.tb2.Name = "tb2"
+        Me.tb2.Size = New System.Drawing.Size(190, 22)
+        Me.tb2.TabIndex = 12
+        '
+        'tb3
+        '
+        Me.tb3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tb3.Location = New System.Drawing.Point(166, 97)
+        Me.tb3.Name = "tb3"
+        Me.tb3.Size = New System.Drawing.Size(190, 22)
+        Me.tb3.TabIndex = 13
+        '
+        'tb4
+        '
+        Me.tb4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tb4.Location = New System.Drawing.Point(166, 125)
+        Me.tb4.Name = "tb4"
+        Me.tb4.Size = New System.Drawing.Size(190, 22)
+        Me.tb4.TabIndex = 14
+        '
+        'tb5
+        '
+        Me.tb5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tb5.Location = New System.Drawing.Point(166, 153)
+        Me.tb5.Name = "tb5"
+        Me.tb5.Size = New System.Drawing.Size(190, 22)
+        Me.tb5.TabIndex = 15
+        '
+        'tb6
+        '
+        Me.tb6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tb6.Location = New System.Drawing.Point(166, 181)
+        Me.tb6.Name = "tb6"
+        Me.tb6.Size = New System.Drawing.Size(190, 22)
+        Me.tb6.TabIndex = 16
+        '
+        'tb7
+        '
+        Me.tb7.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tb7.Location = New System.Drawing.Point(166, 209)
+        Me.tb7.Name = "tb7"
+        Me.tb7.Size = New System.Drawing.Size(190, 22)
+        Me.tb7.TabIndex = 17
+        '
+        'tb8
+        '
+        Me.tb8.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tb8.Location = New System.Drawing.Point(165, 237)
+        Me.tb8.Name = "tb8"
+        Me.tb8.Size = New System.Drawing.Size(190, 22)
+        Me.tb8.TabIndex = 18
+        '
+        'tb9
+        '
+        Me.tb9.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tb9.Location = New System.Drawing.Point(165, 265)
+        Me.tb9.Name = "tb9"
+        Me.tb9.Size = New System.Drawing.Size(190, 22)
+        Me.tb9.TabIndex = 19
+        '
+        'tb10
+        '
+        Me.tb10.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tb10.Location = New System.Drawing.Point(166, 293)
+        Me.tb10.Name = "tb10"
+        Me.tb10.Size = New System.Drawing.Size(190, 22)
+        Me.tb10.TabIndex = 20
+        '
+        'tb11
+        '
+        Me.tb11.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.tb11.Location = New System.Drawing.Point(166, 337)
+        Me.tb11.Name = "tb11"
+        Me.tb11.Size = New System.Drawing.Size(190, 22)
+        Me.tb11.TabIndex = 21
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(422, 377)
+        Me.Controls.Add(Me.tb11)
+        Me.Controls.Add(Me.tb10)
+        Me.Controls.Add(Me.tb9)
+        Me.Controls.Add(Me.tb8)
+        Me.Controls.Add(Me.tb7)
+        Me.Controls.Add(Me.tb6)
+        Me.Controls.Add(Me.tb5)
+        Me.Controls.Add(Me.tb4)
+        Me.Controls.Add(Me.tb3)
+        Me.Controls.Add(Me.tb2)
+        Me.Controls.Add(Me.tb1)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Payroll"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents tb1 As System.Windows.Forms.TextBox
+    Friend WithEvents tb2 As System.Windows.Forms.TextBox
+    Friend WithEvents tb3 As System.Windows.Forms.TextBox
+    Friend WithEvents tb4 As System.Windows.Forms.TextBox
+    Friend WithEvents tb5 As System.Windows.Forms.TextBox
+    Friend WithEvents tb6 As System.Windows.Forms.TextBox
+    Friend WithEvents tb7 As System.Windows.Forms.TextBox
+    Friend WithEvents tb8 As System.Windows.Forms.TextBox
+    Friend WithEvents tb9 As System.Windows.Forms.TextBox
+    Friend WithEvents tb10 As System.Windows.Forms.TextBox
+    Friend WithEvents tb11 As System.Windows.Forms.TextBox
+
+End Class
---
+++ MidtermExam/MidtermExam/Form1.vb
@@ -0,0 +1,121 @@
+﻿Public Class Form1
+    Private EMPLOYEE As String
+    Private HOURS As Integer
+    Private NI, NI2, TD, OT, OTF, SSS, PI, PH, TAX, NP As Double
+
+    Private Sub tb1_TextChanged(sender As Object, e As EventArgs) Handles tb1.TextChanged
+        EMPLOYEE = tb1.Text
+
+    End Sub
+
+    Private Sub tb2_TextChanged(sender As Object, e As EventArgs) Handles tb2.TextChanged
+        HOURS = Val(tb2.Text)
+        If HOURS = 40 Then
+            OT = 0
+            OTF = 0
+
+            NI = HOURS * 108.33
+            tb3.Text = NI
+            tb4.Text = ""
+            tb5.Text = ""
+            SSS = NI * 0.0572
+            tb6.Text = SSS
+            PI = NI * 0.075
+            tb7.Text = PI
+            PH = NI * 0.082
+            tb8.Text = PH
+            TAX = NI * 0.12
+            tb9.Text = TAX
+            TD = SSS + PI + PH + TAX
+            tb10.Text = TD
+
+            NP = NI - TD
+            tb11.Text = NP
+        End If
+
+        If HOURS > 40 Then
+            OT = HOURS - 40
+            tb4.Text = OT
+            OTF = OT * 162.495
+            tb5.Text = OTF
+
+            NI = 40 * 108.33
+            tb3.Text = NI
+
+            NI2 = NI + OTF
+            SSS = NI2 * 0.0572
+            tb6.Text = SSS
+            PI = NI2 * 0.075
+            tb7.Text = PI
+            PH = NI2 * 0.082
+            tb8.Text = PH
+            TAX = NI2 * 0.12
+            tb9.Text = TAX
+            TD = SSS + PI + PH + TAX
+            tb10.Text = TD
+
+            NP = NI2 - TD
+            tb11.Text = NP
+        End If
+        If HOURS < 40 Then
+            tb3.Text = ""
+            tb4.Text = ""
+            tb5.Text = ""
+            tb6.Text = ""
+            tb7.Text = ""
+            tb8.Text = ""
+            tb9.Text = ""
+            tb10.Text = ""
+            tb11.Text = ""
+        End If
+    End Sub
+
+    Private Sub tb3_TextChanged(sender As Object, e As EventArgs) Handles tb3.TextChanged
+        NI = Val(tb3.Text)
+
+
+    End Sub
+
+    Private Sub tb4_TextChanged(sender As Object, e As EventArgs) Handles tb4.TextChanged
+        OT = Val(tb4.Text)
+
+
+    End Sub
+
+    Private Sub tb5_TextChanged(sender As Object, e As EventArgs) Handles tb5.TextChanged
+        OTF = Val(tb5.Text)
+
+
+    End Sub
+
+    Private Sub tb6_TextChanged(sender As Object, e As EventArgs) Handles tb6.TextChanged
+        SSS = Val(tb6.Text)
+
+    End Sub
+
+    Private Sub tb7_TextChanged(sender As Object, e As EventArgs) Handles tb7.TextChanged
+        PI = Val(tb7.Text)
+
+
+    End Sub
+
+    Private Sub tb8_TextChanged(sender As Object, e As EventArgs) Handles tb8.TextChanged
+        PH = Val(tb8.Text)
+
+    End Sub
+
+    Private Sub tb9_TextChanged(sender As Object, e As EventArgs) Handles tb9.TextChanged
+        TAX = Val(tb9.Text)
+
+    End Sub
+
+    Private Sub tb10_TextChanged(sender As Object, e As EventArgs) Handles tb10.TextChanged
+        TD = Val(tb10.Text)
+
+    End Sub
+
+    Private Sub tb11_TextChanged(sender As Object, e As EventArgs) Handles tb11.TextChanged
+        NP = Val(tb11.Text)
+
+    End Sub
+End Class
